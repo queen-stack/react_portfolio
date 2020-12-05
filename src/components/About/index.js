d6e6441cd3553e33c7120997467f8f9bbc17f102
JQueen <queen_jenf@yahoo.com>
2020-12-04 18:23:47 -0800

    add picture

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,21 +10,19 @@ function About() {
         <div className="col-10" id="about-section">
             <img className="mb-5" src="" alt="Jenifer Queen"/>
         <p>
-            I'm baby enamel pin subway tile 90's green juice iPhone 
+        Thrives in high-volume environments and can navigate 
+        evolving business needs, improve client experiences, 
+        and drive profitability. 
         </p>
         <p>
-            Fixie meggings lomo, etsy man bun whatever vice salvia sartorial letterpress. Woke hexagon hoodie coloring book fixie. 
+        Easily understands technical concepts and implications, 
+        manage trade-offs and evaluate new opportunities with stakeholders. 
         </p>
         <p>
-            Edison bulb chambray live-edge godard hexagon. Neutra lomo cred trust fund health goth hoodie cronut raw denim selvage. 
+            Loves to learn new technology!  
+            During COVID, took a course on full stack development.
         </p>
         <p>
-            Church-key before they sold out 8-bit knausgaard forage. Woke hoodie pour-over leggings vinyl etsy fingerstache gluten-free air plant subway tile. 
-        </p>
-        <p>
-            Cloud bread tumblr raclette stumptown kickstarter bespoke bicycle rights butcher meggings edison bulb tacos 8-bit cardigan thundercats pinterest. PBR&B hell of butcher, literally selvage austin flexitarian schlitz letterpress +1 brunch swag 90's poutine XOXO. 
-      </p>
-      <p>
             View full <a href="#/resume" className="link">resume</a>
     </p>
       </div>  
