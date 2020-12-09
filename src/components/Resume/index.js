3afa5fc34d79300c4592e5335824fa5c0993b50a
JQueen <queen_jenf@yahoo.com>
2020-12-08 18:22:10 -0800

    add Heroku

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -16,7 +16,7 @@ return(
                 <p>
                 <a href="/assets/Resume_Queen.pdf" target="_blank" download>resume</a>
                 <br></br>
-                </p>
+                </p>z
             </div>
         </div>
 
